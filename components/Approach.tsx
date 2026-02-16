b9e8dedb65d25cc6537ea18b30f5d579b85ae747
sniper15867 <nehitpahuja@gmail.com>
2026-02-16 15:38:41 +0530

    fixed phone

--- components/Approach.tsx
+++ components/Approach.tsx
@@ -1,5 +1,5 @@
+"use client";
 import React from "react";
-import { AnimatePresence, motion } from "framer-motion";
 
 import { CanvasRevealEffect } from "@/components/ui/CanvasRevealEffect";
 
@@ -79,11 +79,8 @@ const Card = ({
     children?: React.ReactNode;
     des: string;
 }) => {
-    const [hovered, setHovered] = React.useState(false);
     return (
         <div
-            onMouseEnter={() => setHovered(true)}
-            onMouseLeave={() => setHovered(false)}
             // change h-[30rem] to h-[35rem], add rounded-3xl
             className="border border-black/[0.2] group/canvas-card flex items-center justify-center
        dark:border-white/[0.2]  max-w-sm w-full mx-auto p-4 relative lg:h-[35rem] rounded-3xl "
@@ -101,30 +98,22 @@ const Card = ({
             <Icon className="absolute h-10 w-10 -top-3 -right-3 dark:text-white text-black opacity-30" />
             <Icon className="absolute h-10 w-10 -bottom-3 -right-3 dark:text-white text-black opacity-30" />
 
-            <AnimatePresence>
-                {hovered && (
-                    <motion.div
-                        initial={{ opacity: 0 }}
-                        animate={{ opacity: 1 }}
-                        className="h-full w-full absolute inset-0"
-                    >
-                        {children}
-                    </motion.div>
-                )}
-            </AnimatePresence>
+            <div className="h-full w-full absolute inset-0 opacity-100 lg:opacity-0 group-hover/canvas-card:opacity-100 transition duration-200">
+                {children}
+            </div>
 
             <div className="relative z-20 px-10">
                 <div
                     // add this for making it center
                     // absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]
                     className="text-center group-hover/canvas-card:-translate-y-4 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] 
-        group-hover/canvas-card:opacity-0 transition duration-200 min-w-40 mx-auto flex items-center justify-center"
+        group-hover/canvas-card:opacity-0 transition duration-200 min-w-40 mx-auto flex items-center justify-center opacity-0 lg:opacity-100"
                 >
                     {icon}
                 </div>
                 <h2
                     // change text-3xl, add text-center
-                    className="dark:text-white text-center text-3xl opacity-0 group-hover/canvas-card:opacity-100
+                    className="dark:text-white text-center text-3xl opacity-100 lg:opacity-0 group-hover/canvas-card:opacity-100
          relative z-10 text-black mt-4  font-bold group-hover/canvas-card:text-white 
          group-hover/canvas-card:-translate-y-2 transition duration-200"
                 >
@@ -132,7 +121,7 @@ const Card = ({
                 </h2>
                 {/* add this one for the description */}
                 <p
-                    className="text-sm opacity-0 group-hover/canvas-card:opacity-100
+                    className="text-sm opacity-100 lg:opacity-0 group-hover/canvas-card:opacity-100
          relative z-10 mt-4 group-hover/canvas-card:text-white text-center
          group-hover/canvas-card:-translate-y-2 transition duration-200"
                     style={{ color: "#E4ECFF" }}
