1ec5b15043b8b10e22afa98a714ac69b59b0a567
sniper15867 <nehitpahuja@gmail.com>
2026-09-04 04:33:45 +0530

    Removed Framer from tech stack in bento grid

--- components/ui/bento-grid.tsx
+++ components/ui/bento-grid.tsx
@@ -123,7 +123,7 @@ export const BentoGridItem = ({
               </div>
               <div className="flex flex-col gap-3 lg:gap-2">
                 <span className="py-4 px-3 rounded-lg text-center bg-[#10132E]" />
-                {["TailwindCSS", "Framer", "Three.js"].map((item) => (
+                {["TailwindCSS", "Three.js"].map((item) => (
                   <span
                     key={item}
                     className="py-2 lg:py-4 lg:px-5 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]"
